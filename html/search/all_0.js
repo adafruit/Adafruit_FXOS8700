72d84cb44411b3e782b2407ea6ef8cf3ae34af28
Travis CI <travis@travis-ci.org>
2018-01-17 17:34:58 +0000

    Deploy code docs to GitHub Pages Travis build: 4
    Commit: c63b09563df190d0349609ee5b22c552c6354489

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -11,5 +11,5 @@ var searchData=
   ['adafruit_5ffxos8700_2ecpp',['Adafruit_FXOS8700.cpp',['../_adafruit___f_x_o_s8700_8cpp.html',1,'']]],
   ['adafruit_5ffxos8700_2eh',['Adafruit_FXOS8700.h',['../_adafruit___f_x_o_s8700_8h.html',1,'']]],
   ['adafruit_20fxos8700_20accel_2fmag_20sensor_20driver',['Adafruit FXOS8700 accel/mag sensor driver',['../index.html',1,'']]],
-  ['adafruit_5ffxos8700',['Adafruit_FXOS8700',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20fxos8700_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5ffxos8700_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5ffxos8700_29',['Adafruit FXOS8700 Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_FXOS8700.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_FXOS8700)',['../md__home_travis_build_adafruit__adafruit__f_x_o_s8700__r_e_a_d_m_e.html',1,'']]]
 ];
