f99081135200e8b1b583b8143b7b146aa83fcd3a
Doxygen CI <ci-arduino@invalid>
2021-08-04 17:09:49 +0000

    Deploy docs to GitHub Pages from commit 2535991140
    Commit: 253599114056a6e0b7297d689df70e7e30dbd374
    GitHub Actions run: 1098537782

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['setaccelrange',['setAccelRange',['../class_adafruit___f_x_o_s8700.html#aa0d047ab7776e6f15c895e26ab2fc60f',1,'Adafruit_FXOS8700']]],
   ['standby',['standby',['../class_adafruit___f_x_o_s8700.html#afe8bc78aca330e038331ac0ce6fefe35',1,'Adafruit_FXOS8700']]]
 ];
